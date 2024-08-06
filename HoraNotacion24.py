4efbd1d42ff44d3e0d3d39cc023562535b99094e
luis <luis.davila@tecnologicoliceocristiano.edu.ec>
2024-08-05 21:05:05 -0500

    Ejercicioa en clases

---
+++ HoraNotacion24.py
@@ -0,0 +1,16 @@
+hora24=input("Ingrese hora formato 24 horas(HH:MM):")
+partes=hora24.split(":")
+horas=int(partes[0])
+minutos=int(partes[1])
+doce="am"
+if horas>=12:
+    doce="pm"
+if horas>12:
+    horas-=12
+elif horas==12:
+    horas=12
+    print(f"la hora en formato de 12 es {horas}:{minutos:02d}{doce}")
+
+
+
+
